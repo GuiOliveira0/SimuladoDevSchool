987de1bb06d82a5119497ebf5280376f1bb54414
GuiOliveira0 <oliververissimo2004@gmail.com>
2021-09-24 22:43:32 -0300

    ok

--- react/site/src/components/cabecalho/index.js
+++ react/site/src/components/cabecalho/index.js
@@ -8,10 +8,10 @@ export default function Index() {
             <div class="reader-right-box">
                 <div class="box-user"> 
                     <div class="user-image">
-                        <img src="/assets/images/usuario.jpeg" alt="" />
+                        <img src="https://yt3.ggpht.com/ytc/AKedOLQCVxypZ25q1BRyS24WDZS6YiBNiqiyRgUVcQKT7g=s900-c-k-c0x00ffffff-no-rj" alt="" />
                         <div class="absolute">3</div>
                     </div>
-                    <div class="user-name"> Olá, <b>Bruno de Oliveira</b> </div>
+                    <div class="user-name"> Olá, <b>Guilherme Oliveira</b> </div>
                 </div>
                 
                 <div class="box-image">
